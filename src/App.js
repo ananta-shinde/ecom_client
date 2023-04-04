ae520769aac040f1f002c8dcae99864ffcfad753
aShinde <ananta.shinde@honeywellcom>
2023-04-04 10:19:56 +0530

    Product list structure added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './product.json';
+
+const sectionsList = data.sections;
+
+const ProductCard = (props)=>{
+  return(
+    <div className='card w-25'>
+      <img src={props.data.img_url} alt='product-img' width="100%"/>
+      <p>{props.data.title}</p>
+      <p>{props.data.price}</p>
+    </div>
+  )
+}
+const ProductList = (props) =>{
+    return(<div className='d-flex'>
+       {props.productlist.map(p=>(<ProductCard data={p}/>))}
+      </div>)
+}
+
+function ProductSection(props)
+{
+    return(
+      <>
+         <h1>{props.title}</h1>
+         <ProductList productlist = {props.productlist}/>
+      </>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {sectionsList.map(s=>(<ProductSection title={s.sectionTitle} productlist={s.products}/>))}
     </div>
   );
 }
